fc57fbe1fa48cbe4f213d5901d9590a580410aff
Nikita <Novichkoff@icloud.com>
2016-03-03 19:15:27 +0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://ndural.ru/';
+$config['base_url']	= 'http://MY_SITE_NAME/';
 
 /*
 |--------------------------------------------------------------------------
